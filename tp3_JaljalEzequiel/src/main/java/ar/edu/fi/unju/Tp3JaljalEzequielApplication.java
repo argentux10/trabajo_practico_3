08cded84cdc6657d61751cedfacfd2e33b4bd041
Ezequiel Jaljal <ezejaljal2001@hotmail.com>
2024-05-10 22:08:06 -0300

    Se crea el proyecto y el archivo index.html

---
+++ tp3_JaljalEzequiel/src/main/java/ar/edu/fi/unju/Tp3JaljalEzequielApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.fi.unju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3JaljalEzequielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp3JaljalEzequielApplication.class, args);
+	}
+
+}
---
+++ tp3_JaljalEzequiel/src/test/java/ar/edu/fi/unju/Tp3JaljalEzequielApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.fi.unju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3JaljalEzequielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
